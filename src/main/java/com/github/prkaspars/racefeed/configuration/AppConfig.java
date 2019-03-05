a211d91b38adb4a5e4c074d88ca6765190303ad4
Kaspars Primaks <kaspars.primaks@gmail.com>
2019-03-05 17:09:50 +0000

    remove queues form config

--- src/main/java/com/github/prkaspars/racefeed/configuration/AppConfig.java
+++ src/main/java/com/github/prkaspars/racefeed/configuration/AppConfig.java
@@ -1,22 +1,20 @@
 package com.github.prkaspars.racefeed.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.prkaspars.racefeed.message.CarStatus;
-import com.github.prkaspars.racefeed.message.Event;
 import com.github.prkaspars.racefeed.model.Car;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.LinkedTransferQueue;
-import java.util.concurrent.TransferQueue;
+import java.util.List;
+import java.util.function.Predicate;
 
 @Configuration
 @EnableConfigurationProperties(MqttConfig.class)
@@ -42,27 +40,18 @@ public class AppConfig {
   }
 
   @Bean
-  public Executor executor() {
+  public ThreadPoolTaskExecutor executor() {
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-    executor.setCorePoolSize(6);
-    executor.setMaxPoolSize(6 + 2);
+    executor.setCorePoolSize(12);
+    executor.setMaxPoolSize(12);
     executor.setThreadNamePrefix("ex");
     executor.initialize();
     return executor;
   }
 
   @Bean
-  public TransferQueue<Car.State> states() {
-    return new LinkedTransferQueue<>();
-  }
-
-  @Bean
-  public TransferQueue<CarStatus> statuses() {
-    return new LinkedTransferQueue<>();
-  }
-
-  @Bean
-  public TransferQueue<Event> events() {
-    return new LinkedTransferQueue<>();
+  @Qualifier("rankingPredicate")
+  public Predicate<List<Car.State>> rankingPredicate() {
+    return l -> l.size() == 6;
   }
 }
