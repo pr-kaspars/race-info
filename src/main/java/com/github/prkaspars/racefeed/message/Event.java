bf3c711bbdb91ecaf4bddddcb13b99b295e6bbe0
Kaspars Primaks <kaspars.primaks@gmail.com>
2019-03-05 17:09:21 +0000

    add message factory method

--- src/main/java/com/github/prkaspars/racefeed/message/Event.java
+++ src/main/java/com/github/prkaspars/racefeed/message/Event.java
@@ -4,6 +4,10 @@ public class Event {
   private long timestamp;
   private String text;
 
+  public static Event newDramaticOvertake(long timestamp, int c1, int c2) {
+    return new Event(timestamp, String.format("Car %d races ahead of Car %d in a dramatic overtake.", c1, c2));
+  }
+
   public Event(long timestamp, String text) {
     this.timestamp = timestamp;
     this.text = text;
